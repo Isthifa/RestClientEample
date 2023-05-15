de3176759aa240816ed6796ae89d01f80522a8e2
isthifa <4pa19cs404@pace.edu.in>
2023-05-15 11:58:17 +0530

    role based

--- Spring_Security_Basic/src/main/java/com/example/spring_security_basic/controller/examplerest.java
+++ Spring_Security_Basic/src/main/java/com/example/spring_security_basic/controller/examplerest.java
@@ -1,13 +1,19 @@
 package com.example.spring_security_basic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class examplerest {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayhello()
     {
         return "Good morning";
+    }
+    @GetMapping("/admin")
+    public String admin()
+    {
+        return "Hello Admin";
     }
 }
