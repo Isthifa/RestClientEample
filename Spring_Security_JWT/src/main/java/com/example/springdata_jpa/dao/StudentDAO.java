ed7b1f48b81ca593cc9dbdf7d65033ebf752a006
Isthifa <87200953+Isthifa@users.noreply.github.com>
2023-09-20 12:33:26 +0530

    Delete Spring_Security_JWT directory

--- Spring_Security_JWT/src/main/java/com/example/springdata_jpa/dao/StudentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdata_jpa.dao;
-
-import com.example.springdata_jpa.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentDAO extends JpaRepository<Student,Long> {
-}
